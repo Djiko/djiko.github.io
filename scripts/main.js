d9542a370e5047c2a97fbc59c1383a7d0e09db60
Gilles Coulais <gilles@coulais.net>
2022-01-02 16:10:37 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/480px-Frowny.svg.png') {
+        myImage.setAttribute('src', 'images/480px-Smiley.svg.png')
+    } else {
+        myImage.setAttribute('src', 'images/480px-Frowny.svg.png')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;    
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
